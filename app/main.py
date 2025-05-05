abf8d666041f36bf7414db9a585a2d5567bce3d2
Vitalii Mushta <vitaliimushta1993@gmail.com>
2025-05-05 12:49:24 +0200

    solution3

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-import os
-
-
 def copy_file(command: str) -> None:
     command_lst = command.split()
-    if ((len(command_lst) == 3 and command_lst[0] == "cp"
-         and os.path.isfile(command_lst[1]))
-            and command_lst[1] != command_lst[2]):
-        with (open(command_lst[1], "rb") as f,
-              open(command_lst[2], "wb") as f_copy):
-            for line in f.readlines():
-                f_copy.write(line)
+    if len(command_lst) != 3:
+        print("You must give argument in format "
+              "(command, file name, new file for copy)")
+        return
+    if command_lst[0] != "cp":
+        print("command must be 'CP'")
+        return
+    if command_lst[1] != command_lst[2]:
+        try:
+            with (open(command_lst[1], "rb") as f,
+                  open(command_lst[2], "wb") as f_copy):
+                for line in f.readlines():
+                    f_copy.write(line)
+        except FileNotFoundError as e:
+            print(f"{e}")
+    return
