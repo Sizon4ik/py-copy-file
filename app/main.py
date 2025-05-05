8e892933356d8c05dadf4668163037406691ebb9
Vitalii Mushta <vitaliimushta1993@gmail.com>
2025-05-05 12:28:39 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    command_lst = command.split()
+    if (len(command_lst) == 3 and command_lst[0] == "cp"
+            and os.path.isfile(command_lst[1])):
+        if command_lst[1] != command_lst[2]:
+            with (open(command_lst[1], "r") as f,
+                  open(command_lst[2], "w") as f_copy):
+                for line in f.readlines():
+                    f_copy.write(line)
