c81bce5efdd51f6e9e3cc2830c91cb5bcae0e2cb
Vitalii Mushta <vitaliimushta1993@gmail.com>
2025-05-05 12:35:27 +0200

    new_solution

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ import os
 
 def copy_file(command: str) -> None:
     command_lst = command.split()
-    if (len(command_lst) == 3 and command_lst[0] == "cp"
-            and os.path.isfile(command_lst[1])):
-        if command_lst[1] != command_lst[2]:
-            with (open(command_lst[1], "r") as f,
-                  open(command_lst[2], "w") as f_copy):
-                for line in f.readlines():
-                    f_copy.write(line)
+    if ((len(command_lst) == 3 and command_lst[0] == "cp"
+         and os.path.isfile(command_lst[1]))
+            and command_lst[1] != command_lst[2]):
+        with (open(command_lst[1], "rb") as f,
+              open(command_lst[2], "wb") as f_copy):
+            for line in f.readlines():
+                f_copy.write(line)
